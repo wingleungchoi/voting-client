bcde5d42c92c02a105b6e89e2734dc3d732054bc
wingleungchoi <wingleungchoi@gmail.com>
2016-09-10 23:56:12 +0800

    work now

--- src/index.jsx
+++ src/index.jsx
@@ -11,12 +11,13 @@ import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
+const socket = io('http://localhost:8090');//`${location.protocol}//${location.hostname}:8090`);
+
 const createStoreWithMiddleware = applyMiddleware(
   remoteActionMiddleware(socket)
 )(createStore);
 const store = createStoreWithMiddleware(reducer);
 
-const socket = io('http://localhost:8090');//`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
   store.dispatch(setState(state))
 );
