7db5473dc0ed621fdb4befc3bb7061867f1ac36b
wingleungchoi <wingleungchoi@gmail.com>
2016-09-25 16:51:02 +0800

    add clientId

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,9 @@ import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import io from 'socket.io-client';
 import reducer from './reducer';
-import {setState} from './action_creators';
+import {setClientId, setState} from './action_creators';;
 import remoteActionMiddleware from './remote_action_middleware';
+import getClientId from './client_id';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
@@ -17,6 +18,7 @@ const createStoreWithMiddleware = applyMiddleware(
   remoteActionMiddleware(socket)
 )(createStore);
 const store = createStoreWithMiddleware(reducer);
+store.dispatch(setClientId(getClientId()));
 
 socket.on('state', state =>
   store.dispatch(setState(state))
